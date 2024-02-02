8bf2cfd6425a8d09e90bcb63eb05f6066426ae96
dchristiansen <dylanjay007@gmail.com>
2024-02-01 23:45:35 -0600

    test: fix failing tests

--- test/unit_tests/main_test.py
+++ test/unit_tests/main_test.py
@@ -33,18 +33,21 @@ def test_update_message_content_appends_role_data_to_first_message():
     assert len(data.messages) == 1
 
 
-# TODO: fix this testgit
 def test_update_message_content_appends_sys_info_to_first_message_if_first_message():
     role_data = ""
     data = OpenAIRequest(
-        messages=[{"role": "user", "content": "Hello"}], request_context=None
+        messages=[
+            {"role": "system", "content": ""},
+            {"role": "user", "content": "Hello"},
+        ],
     )
     sys_info_details = {"key": "value"}
     update_message_content(data, sys_info_details, role_data)
-    expected_content = f"Hello\nHere is information about my computer:\n{SYS_INFO_KEY}: {sys_info_details}\n"
-    print(f"Test test test: {data.messages[0]}")
-    assert data.messages[0]["content"] == expected_content
-    assert len(data.messages) == 1
+    expected_content = f"\nHere is information about my computer:\n \
+        {SYS_INFO_KEY}: {sys_info_details}\n"
+    update_data = data.messages[0]["content"]
+    assert update_data == expected_content
+    assert len(data.messages) == 2
 
 
 def test_update_message_content_does_not_append_sys_info_if_not_first_message():
@@ -61,35 +64,35 @@ def test_update_message_content_does_not_append_sys_info_if_not_first_message():
 async def test_request_context_not_provided__no_additional_info_added():
     messages = [
         {"role": "system", "content": "I am a helpful assistant"},
-        {"role": "user", "content": "Hello"}
+        {"role": "user", "content": "Hello"},
     ]
     data = OpenAIRequest(messages=messages)
-    with patch('openai.chat.completions.create'):
+    with patch("openai.chat.completions.create"):
         await ask_openai(data)
         assert data.messages[0]["content"] == "I am a helpful assistant"
 
 
 @pytest.mark.asyncio
 async def test_request_context_provided__info_added_to_first_system_message():
-    request_context = {"sys_info": ["{type, linux}"]}
-    request_context_str = ["sys_info", "type", "linux"]
+    request_context = {"sys_info": {"os": "linux", "shell": "bash"}}
+    request_context_str = ["sys_info: {'os': 'linux', 'shell': 'bash'}"]
     messages = [
-        {"role": "user", "content": "Hello"}
+        {"role": "system", "content": "I am a helpful assistant"},
+        {"role": "user", "content": "Hello"},
     ]
     data = OpenAIRequest(messages=messages, request_context=request_context)
-    with patch('openai.chat.completions.create') as mock_openai_call:
+    with patch("openai.chat.completions.create") as mock_openai_call:
         mock_openai_call.return_value = mock_openai_response_200
         await ask_openai(data)
         mock_openai_call.assert_called_once()
         called_with_messages = mock_openai_call.call_args[1]["messages"]
         for contextStr in request_context_str:
-            assert contextStr in called_with_messages[0]['content']
+            assert contextStr in called_with_messages[0]["content"]
 
 
 @pytest.mark.asyncio
 async def test_ask_openai__success__return_json():
-    with patch('openai.chat.completions.create',
-               return_value=mock_openai_response_200):
+    with patch("openai.chat.completions.create", return_value=mock_openai_response_200):
         data = OpenAIRequest(messages=[{"role": "user", "content": "Hello"}])
         response = await ask_openai(data)
         assert response.status_code == 200
@@ -97,8 +100,9 @@ async def test_ask_openai__success__return_json():
 
 @pytest.mark.asyncio
 async def test_ask_openai__openai_server_issue__502_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_502_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_502_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -107,8 +111,9 @@ async def test_ask_openai__openai_server_issue__502_inform_client():
 
 @pytest.mark.asyncio
 async def test_ask_openai__bungo_server_issue__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_500_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_500_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -117,8 +122,9 @@ async def test_ask_openai__bungo_server_issue__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_ask_openai__malformed_openai_request__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_400_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_400_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -127,8 +133,9 @@ async def test_ask_openai__malformed_openai_request__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_ask_openai__malformed_openai_auth__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_401_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_401_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -137,8 +144,9 @@ async def test_ask_openai__malformed_openai_auth__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_ask_openai__openai_denies_access__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_403_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_403_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -147,8 +155,9 @@ async def test_ask_openai__openai_denies_access__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_ask_openai__no_openai_resource__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_404_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_404_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -157,8 +166,9 @@ async def test_ask_openai__no_openai_resource__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_openai_reports_unprocessable_entity__500_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_422_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_422_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
@@ -167,8 +177,9 @@ async def test_openai_reports_unprocessable_entity__500_inform_client():
 
 @pytest.mark.asyncio
 async def test_openai_reports_rate_limit_reached__529_inform_client():
-    with patch('openai.chat.completions.create',
-               side_effect=mock_openai_response_429_error):
+    with patch(
+        "openai.chat.completions.create", side_effect=mock_openai_response_429_error
+    ):
         data = OpenAIRequest(messages=[])
         with pytest.raises(HTTPException) as exc_info:
             await ask_openai(data)
