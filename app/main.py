555cc7a53347445a3a22e1a3a38369dab32a6ae7
Dylan Christiansen <dylanjay007@gmail.com>
2024-02-04 20:52:58 -0600

    fix: Fix/open api key (#17)
    * fix: main code to read OpenAI API key from file
    
    * fix: exporting env var so local tests work

--- app/main.py
+++ app/main.py
@@ -25,7 +25,11 @@ app.add_middleware(
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
-    raise Exception("OPENAI_API_KEY not found in environment variables")
+    raise ValueError(
+        "OpenAI API key not found. Make sure the environment\
+         variable OPENAI_API_KEY is set."
+    )
+
 
 openai.api_key = OPENAI_API_KEY
 
@@ -34,19 +38,21 @@ async def call_openai_api(data: OpenAIRequest):
     is_first_message = len(data.messages) == 1
 
     sys_info_key = "sys_info"
-    context_has_sys_info = (data.request_context and
-                            sys_info_key in data.request_context.keys())
+    context_has_sys_info = (
+        data.request_context and sys_info_key in data.request_context.keys()
+    )
 
-    if (is_first_message and context_has_sys_info):
+    if is_first_message and context_has_sys_info:
         sys_info_details = data.request_context[sys_info_key]
-        data.messages[0]['content'] += f"""
+        data.messages[0][
+            "content"
+        ] += f"""
         Here is information about my computer:\n
         {sys_info_key}: {sys_info_details}
         """
     try:
         response = openai.chat.completions.create(
-            model="gpt-4-turbo-preview",
-            messages=data.messages
+            model="gpt-4-turbo-preview", messages=data.messages
         )
         return response
 
@@ -57,51 +63,39 @@ async def call_openai_api(data: OpenAIRequest):
 def HandleOpenAICallFailures(e: Exception):
     if isinstance(e, openai.BadRequestError):
         raise HTTPException(
-            500,
-            "OpenAI Request denied due to bad request: {}"
-            .format(e.response)
+            500, "OpenAI Request denied due to bad request: {}".format(e.response)
         )
     elif isinstance(e, openai.AuthenticationError):
         raise HTTPException(
             500,
-            "OpenAI Request denied due to authentication issue: {}"
-            .format(e.response)
+            "OpenAI Request denied due to authentication issue: {}".format(e.response),
         )
     elif isinstance(e, openai.PermissionDeniedError):
         raise HTTPException(
-            500,
-            "OpenAI Request denied due to permission denied: {}"
-            .format(e.response)
+            500, "OpenAI Request denied due to permission denied: {}".format(e.response)
         )
     elif isinstance(e, openai.NotFoundError):
         raise HTTPException(
             500,
-            "OpenAI Request denied due to resource not found: {}"
-            .format(e.response)
+            "OpenAI Request denied due to resource not found: {}".format(e.response),
         )
     elif isinstance(e, openai.UnprocessableEntityError):
         raise HTTPException(
             522,
-            "OpenAI Request denied due to unprocessable entity: {}"
-            .format(e.response)
+            "OpenAI Request denied due to unprocessable entity: {}".format(e.response),
         )
     elif isinstance(e, openai.RateLimitError):
         raise HTTPException(
             529,
-            "OpenAI Request denied due to hitting rate limit: {}"
-            .format(e.response)
+            "OpenAI Request denied due to hitting rate limit: {}".format(e.response),
         )
     elif isinstance(e, openai.APIConnectionError):
         raise HTTPException(
-            500,
-            "The OpenAI server could not be reached: {}"
-            .format(e.__cause__)
+            500, "The OpenAI server could not be reached: {}".format(e.__cause__)
         )
     elif isinstance(e, openai.InternalServerError):
         raise HTTPException(
-            502,
-            "The OpenAI server had an internal error: {}"
-            .format(e.__cause__)
+            502, "The OpenAI server had an internal error: {}".format(e.__cause__)
         )
     else:
         raise e  # Re-raise the exception if it's not one of the known types
