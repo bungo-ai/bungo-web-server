5e1e3dfab33045998321c760e4c25e07c429ac1d
dchristiansen <dylanjay007@gmail.com>
2024-02-04 19:57:33 -0600

    fix: main code to read OpenAI API key from file

--- app/main.py
+++ app/main.py
@@ -15,9 +15,19 @@ class OpenAIRequest(BaseModel):
 
 app = FastAPI()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY_FILE = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY_FILE:
+    raise ValueError(
+        "OpenAI API key file path not found. Make sure the environment\
+         variable OPENAI_API_KEY is set."
+    )
+
+with open(OPENAI_API_KEY_FILE, "r") as key_file:
+    OPENAI_API_KEY = key_file.read().strip()
+
 if not OPENAI_API_KEY:
-    raise Exception("OPENAI_API_KEY not found in environment variables")
+    raise ValueError(f"OpenAI API key not found in the {OPENAI_API_KEY_FILE}.")
+
 
 openai.api_key = OPENAI_API_KEY
 
@@ -26,19 +36,21 @@ async def call_openai_api(data: OpenAIRequest):
     is_first_message = len(data.messages) == 2
 
     sys_info_key = "sys_info"
-    context_has_sys_info = (data.request_context and
-                            sys_info_key in data.request_context.keys())
+    context_has_sys_info = (
+        data.request_context and sys_info_key in data.request_context.keys()
+    )
 
-    if (is_first_message and context_has_sys_info):
+    if is_first_message and context_has_sys_info:
         sys_info_details = data.request_context[sys_info_key]
-        data.messages[0]['content'] += f"""
+        data.messages[0][
+            "content"
+        ] += f"""
         Here is information about my computer:\n
         {sys_info_key}: {sys_info_details}
         """
     try:
         response = openai.chat.completions.create(
-            model="gpt-4-turbo-preview",
-            messages=data.messages
+            model="gpt-4-turbo-preview", messages=data.messages
         )
         return response
 
@@ -49,51 +61,39 @@ async def call_openai_api(data: OpenAIRequest):
 def HandleOpenAICallFailures(e: Exception):
     if isinstance(e, openai.BadRequestError):
         raise HTTPException(
-            500,
-            "OpenAI Request denied due to bad request: {}"
-            .format(e.response)
+            500, "OpenAI Request denied due to bad request: {}".format(e.response)
         )
     elif isinstance(e, openai.AuthenticationError):
         raise HTTPException(
             500,
-            "OpenAI Request denied due to authentication issue: {}"
-            .format(e.response)
+            "OpenAI Request denied due to authentication issue: {}".format(e.response),
         )
     elif isinstance(e, openai.PermissionDeniedError):
         raise HTTPException(
-            500,
-            "OpenAI Request denied due to permission denied: {}"
-            .format(e.response)
+            500, "OpenAI Request denied due to permission denied: {}".format(e.response)
         )
     elif isinstance(e, openai.NotFoundError):
         raise HTTPException(
             500,
-            "OpenAI Request denied due to resource not found: {}"
-            .format(e.response)
+            "OpenAI Request denied due to resource not found: {}".format(e.response),
         )
     elif isinstance(e, openai.UnprocessableEntityError):
         raise HTTPException(
             522,
-            "OpenAI Request denied due to unprocessable entity: {}"
-            .format(e.response)
+            "OpenAI Request denied due to unprocessable entity: {}".format(e.response),
         )
     elif isinstance(e, openai.RateLimitError):
         raise HTTPException(
             529,
-            "OpenAI Request denied due to hitting rate limit: {}"
-            .format(e.response)
+            "OpenAI Request denied due to hitting rate limit: {}".format(e.response),
         )
     elif isinstance(e, openai.APIConnectionError):
         raise HTTPException(
-            500,
-            "The OpenAI server could not be reached: {}"
-            .format(e.__cause__)
+            500, "The OpenAI server could not be reached: {}".format(e.__cause__)
         )
     elif isinstance(e, openai.InternalServerError):
         raise HTTPException(
-            502,
-            "The OpenAI server had an internal error: {}"
-            .format(e.__cause__)
+            502, "The OpenAI server had an internal error: {}".format(e.__cause__)
         )
     else:
         raise e  # Re-raise the exception if it's not one of the known types
